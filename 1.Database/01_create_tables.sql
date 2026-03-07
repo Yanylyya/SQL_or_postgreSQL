7cba8e924234b91e6858efc89378635a90cdc42f
Yana Beleiovych <208520423+Yanylyya@users.noreply.github.com>
2026-03-07 19:43:50 +0100

    Create initial database tables for book management
    Added tables for books, authors, genres, languages, and their relationships.

--- 1.Database/01_create_tables.sql
+++ 1.Database/01_create_tables.sql
@@ -1,1 +1,30 @@
+create table books (
+id int primary key,
+title varchar, 
+publication_year int,
+pages int );
+  
+create table authors (
+id int primary key,
+name varchar, 
+coutry varchar );
+  
+create table genres (
+id int primary key,
+name varchar);
+  
+create table languages (
+id int primary key,
+name varchar);
 
+create table book_authors (
+book_id int, 
+authors_id int);
+  
+create table book_genres (
+book_id int, 
+genre_id int);
+
+create table book_languages (
+book_id int, 
+language_id int)
